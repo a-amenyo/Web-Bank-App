ecc3688e0ea16f5befee0bc115e031fc9958630d
yoooel <72613221+yoooel@users.noreply.github.com>
2021-03-19 15:35:41 +0000

    E-banking page pics added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-// const imgSlides = document.querySelector('.img-slides');
-// const carouselImages = document.querySelectorAll('img-slides img');
- 
-//buttons
-// const prevBtn = document.querySelector('prev-btn');
-// const nextBtn = document.querySelector('nxt-btn');
-
-//Counter
-// let counter = 1;
-// const size = carouselImages[0].clientWidth;
-
-// imgSlides.getElementsByClassName.transform = 'translateX(' +( - size * counter ) + 'px)' ;
-
-//button listeners
-// nextBtn.addEventListener('click' , ()=>{
-//   imgSlides.style.transition= "transform 0.5s ease-in-out";
-//   counter ++;
-//   imgSlides.getElementsByClassName.transform = 'translateX(' +( - size * counter ) + 'px)' ;
-//   console.log(counter);
-
-// });
-
+// Home Page Slider
 var slideIndex = 1;
 showSlides(slideIndex);
 
